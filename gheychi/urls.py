c526126ad9e0c23ceaa69a701dc3eea7d79ba911
amirali-lll <amirali.dst.lll@gmail.com>
2024-02-28 15:27:21 +0330

    add swagger documentation to project

--- gheychi/urls.py
+++ gheychi/urls.py
@@ -2,8 +2,26 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Gheychi API",
+      default_version='v1',
+      description="The Gheychi API from NAGZAP Group \n from IUST university",
+      terms_of_service="https://gheychi.fiust.ir",
+      contact=openapi.Contact(email="amirali.dst.lll@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('',include('core.urls')),
 ]\
