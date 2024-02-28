6e37dd42cc2cfe2d0b24a4244d15562d85e2aecf
amirali-lll <amirali.dst.lll@gmail.com>
2024-02-28 16:20:18 +0330

    change the admin url for security reasons

--- gheychi/urls.py
+++ gheychi/urls.py
@@ -22,7 +22,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('admin/', admin.site.urls),
+    path('raamfar/', admin.site.urls),
     path('',include('core.urls')),
 ]\
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)\
