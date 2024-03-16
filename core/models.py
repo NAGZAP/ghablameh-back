aea0e08435196a53eb38893bf816773431054571
amirali-lll <amirali.dst.lll@gmail.com>
2024-03-16 20:44:00 +0330

    create User Model

--- core/models.py
+++ core/models.py
@@ -1,5 +1,27 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.core.validators import RegexValidator
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
-    pass
+
+    phone_number = models.CharField(
+        max_length=20,
+        validators=[
+            RegexValidator(
+                regex=r'^\+?\d{12,15}$',
+                message="شماره تلفن باید به شکل +989123456789 وارد شود"
+            ),
+        ],
+        blank=True,
+        null=True
+    )
+    
+    GENDER_CHOICES = (
+        ('M', _('Male')),
+        ('F', _('Female')),
+        ('O', _('Other')),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, null=True, blank=True)
+    
