1c9fe8d34091cd3594e3b66937cbfd87f859ff74
amirali-lll <amirali.dst.lll@gmail.com>
2024-03-25 11:23:24 +0330

    add base url to help to call endpoints and clean code

--- ghablameh/urls.py
+++ ghablameh/urls.py
@@ -7,10 +7,12 @@ from .swagger import schema_view
 VERSION = settings.VERSION
 
 
+BASE_URL = f"api/v{VERSION}"
+
 urlpatterns = [
-    path(f'api/v{VERSION}/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path(f'api/v{VERSION}/raamfar', admin.site.urls),
-    path(f'api/v{VERSION}',include('core.urls')),
+    path(f'{BASE_URL}/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path(f'{BASE_URL}/raamfar', admin.site.urls),
+    path(f'{BASE_URL}',include('core.urls')),
 ]\
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)\
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
