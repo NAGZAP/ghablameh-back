7613d5dab7eceeb9b7b3b6e5a995cc7dedc5a07d
amirali-lll <amirali.dst.lll@gmail.com>
2024-03-27 18:09:39 +0330

    add the food_reservation urls to base urls

--- ghablameh/urls.py
+++ ghablameh/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path(f'{BASE_URL}/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path(f'{BASE_URL}/raamfar', admin.site.urls),
     path(f'{BASE_URL}/',include('core.urls')),
+    path(f'{BASE_URL}/food-reservation',include('food_reservation.urls')),
 ]\
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)\
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
