2396d5e82d1580f778b69ca81bc99415dd53a299
amirali-lll <amirali.dst.lll@gmail.com>
2024-04-04 07:31:45 +0330

    add JWT authentication to swagger

--- ghablameh/swagger.py
+++ ghablameh/swagger.py
@@ -1,7 +1,7 @@
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -11,7 +11,11 @@ schema_view = get_schema_view(
       terms_of_service="https://Ghablameh.fiust.ir",
       contact=openapi.Contact(email="amirali.dst.lll@gmail.com"),
       license=openapi.License(name="BSD License"),
+      x={
+         'security': [{'Bearer': []}],
+      },
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
+   authentication_classes=(JWTAuthentication,),
 )
