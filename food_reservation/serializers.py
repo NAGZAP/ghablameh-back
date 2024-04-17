0a452813d6dad5e805068125a055d4fbadc9a98a
amirali-lll <amirali.dst.lll@gmail.com>
2024-04-17 18:23:47 +0330

    Update URLs and serializers in food_reservation app to handle join requests

--- food_reservation/serializers.py
+++ food_reservation/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from .models import Buffet
+from .models import Buffet,Organization,OrganizationMemberShipRequest,OrganizationMemberShipInvitation
 
 
 User = get_user_model()
@@ -27,3 +27,50 @@ class BuffetListSerializer(serializers.ModelSerializer):
     class Meta:
         model  = Buffet
         fields = ['id','name']
+
+
+
+class MemberShipRequestSerializer(serializers.ModelSerializer):
+    client_name = serializers.CharField(source='client.user.username', read_only=True)
+    organization_name = serializers.CharField(source='organization.name', read_only=True)
+
+    class Meta:
+        model  = OrganizationMemberShipRequest
+        fields = ['id','client_name', 'organization_name', 'status', 'created_at', 'updated_at']
+
+    def update(self, instance, validated_data):
+        # if the request is accepted, add the client to the organization    
+        if validated_data['status'] == 'A':
+            instance.organization.members.add(instance.client)
+        return super().update(instance, validated_data)
+
+
+class CreateMemberShipRequestSerializer(serializers.ModelSerializer):
+    client_name = serializers.CharField(source='client.user.username', read_only=True)
+    organization_name = serializers.CharField(source='organization.name', read_only=True)
+
+    class Meta:
+        model  = OrganizationMemberShipRequest
+        fields = ['id', 'organization', 'created_at', 'updated_at', 'client_name', 'organization_name']
+
+    def save(self, **kwargs):
+        return super().save(**kwargs)
+    
+    # TODO validate that the client is not already a member of the organization
+
+    def create(self, validated_data):
+        request = OrganizationMemberShipRequest.objects.filter(
+            client=validated_data['client'], organization=validated_data['organization']
+        ).first()
+        if request:
+            return request
+        return super().create(validated_data)
+
+
+class MemberShipInvitationSerializer(serializers.ModelSerializer):
+    client_name = serializers.CharField(source='client.user.username', read_only=True)
+    organization_name = serializers.CharField(source='organization.name', read_only=True)
+
+    class Meta:
+        model  = OrganizationMemberShipInvitation
+        fields = ['id','client','client_name', 'organization', 'organization_name', 'status', 'created_at', 'updated_at']
