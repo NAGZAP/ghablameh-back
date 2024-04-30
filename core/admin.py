5d4f0e9b9b10d7cf071ac50616c34553cd170323
amirali-lll <amirali.dst.lll@gmail.com>
2024-04-30 17:06:18 +0330

    chore: Register User and Notification models in admin site and add admin actions

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'phone_number', 'is_staff', 'is_active']
+    list_filter = ['is_staff', 'is_active']
+    search_fields = ['username', 'email', 'phone_number']
+    ordering = ['username']
+    actions = ['make_staff', 'make_active', 'make_inactive']
+    
+    def make_staff(self, request, queryset):
+        queryset.update(is_staff=True)
+    
+    def make_active(self, request, queryset):
+        queryset.update(is_active=True)
+    
+    def make_inactive(self, request, queryset):
+        queryset.update(is_active=False)
+    
+    make_staff.short_description = "Make selected users staff"
+    make_active.short_description = "Make selected users active"
+    make_inactive.short_description = "Make selected users inactive"
--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Notification
 
-# Register your models here.
+
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ['user', 'title', 'created_at', 'read']
+    list_filter = ['read']
+    search_fields = ['title', 'message']
+    ordering = ['-created_at']
+    actions = ['mark_as_read', 'mark_as_unread']
+    autocomplete_fields = ['user']
+
+    list_select_related = ['user']
+
+    
+    def mark_as_read(self, request, queryset):
+        queryset.update(read=True)
+    
+    def mark_as_unread(self, request, queryset):
+        queryset.update(read=False)
