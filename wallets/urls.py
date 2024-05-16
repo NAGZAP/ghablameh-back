6991d86bfd5d9c9a0df2b5267a1148901f817ba9
amirali-lll <amirali.dst.lll@gmail.com>
2024-05-16 20:21:01 +0330

    Add GET wallet api

--- wallets/urls.py
+++ wallets/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 router = DefaultRouter()
 
+router.register(r'', views.WalletViewSet, basename='wallets')
 
 
 urlpatterns = router.urls
--- wallets/views.py
+++ wallets/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .serializers import *
 
-# Create your views here.
+
+
+
+
+class WalletViewSet(GenericViewSet):
+    
+    serializer_class = WalletSerializer
+    
+    @action(methods=['get'], detail=False)
+    def me(self, request):
+        wallet = Wallet.objects.get(user=request.user)
+        serializer = WalletSerializer(wallet)
+        return Response(serializer.data)
