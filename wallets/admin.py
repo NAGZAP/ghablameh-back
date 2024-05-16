e1170b49da4b2e434fa6bc506b3038f08ca187cf
amirali-lll <amirali.dst.lll@gmail.com>
2024-05-16 20:20:39 +0330

    Add admin panel configs for wallet

--- wallets/admin.py
+++ wallets/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Wallet 
 
-# Register your models here.
+
+
+@admin.register(Wallet)
+class WalletAdmin(admin.ModelAdmin):
+    list_display = ['user', 'balance', 'created_at', 'updated_at']
+    search_fields = ['user__username']
+    ordering = ['created_at']
+    actions = ['zero_balance']
+    
+    def zero_balance(self, request, queryset):
+        queryset.update(balance=0)
+        self.message_user(request, "Balance of selected wallets has been set to zero.")
+    zero_balance.short_description = "Set balance to zero"
+    
