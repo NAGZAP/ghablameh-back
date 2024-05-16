f7d59fe76e08862c5dee2ed898be77e4a96c21f5
amirali-lll <amirali.dst.lll@gmail.com>
2024-05-16 20:19:45 +0330

    Add Wallet Model

--- wallets/models.py
+++ wallets/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+
+class Wallet(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT, related_name='wallet')
+    balance = models.DecimalField(max_digits=12, decimal_places=0, default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.user.username
