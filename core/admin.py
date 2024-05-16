340c3512b4b64506ede97f5370510973f69ddbcc
amirali-lll <amirali.dst.lll@gmail.com>
2024-05-16 22:32:00 +0330

    Change the MyOrganization View set and the pagination and filter to it

--- core/admin.py
+++ core/admin.py
@@ -7,4 +7,6 @@ User = get_user_model()
 
 @admin.register(User)
 class UserAdmin(UserAdmin):
-    list_display = ['id','username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser']
+    list_display = ['id','username', 'email', 'first_name', 'last_name', 'is_verified']
+    list_editable = ['is_verified']
+
---
+++ food_reservation/filters.py
@@ -0,0 +1,19 @@
+from django_filters import rest_framework as filters, NumberFilter
+from django.db.models import Avg
+from .models import Organization
+
+class OrganizationFilter(filters.FilterSet):
+    average_rate_gte = NumberFilter(field_name='average_rate', lookup_expr='gte')
+    average_rate_lte = NumberFilter(field_name='average_rate', lookup_expr='lte')
+
+    class Meta:
+        model = Organization
+        fields = {
+            'name': ['icontains'],
+        }
+
+    def filter_by_average_rate(self, queryset, name, value):
+        return queryset.annotate(average_rate=Avg('buffets__rates__rate')).filter(average_rate=value)
+
+    def get_queryset(self):
+        return super().get_queryset().annotate(average_rate=Avg('buffets__rates__rate'))
