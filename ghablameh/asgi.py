f9040aff1671d7ba48e41a6486c68e2552ec91d9
amirali-lll <amirali.dst.lll@gmail.com>
2024-05-16 23:08:54 +0330

    Fix the issue in routing ws

--- ghablameh/asgi.py
+++ ghablameh/asgi.py
@@ -6,11 +6,20 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.urls import path
+from notifications import consumers
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ghablameh.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            path('ws/notifications/', consumers.NotificationConsumer.as_asgi()),
+        ])
+    ),
+})
