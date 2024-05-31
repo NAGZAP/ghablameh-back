10eeae7688473685f95f52ca35c94a80e5199530
amirali-lll <amirali.dst.lll@gmail.com>
2024-05-31 23:00:56 +0330

    add endpoints of charging

--- wallets/serializers.py
+++ wallets/serializers.py
@@ -9,3 +9,21 @@ class WalletSerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_fields = ['user', 'balance', 'created_at', 'updated_at']
         
+        
+        
+class DepositSerializer(serializers.Serializer):
+    amount = serializers.DecimalField(max_digits=11, decimal_places=0)
+    
+    def validate_amount(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Amount must be greater than 0")
+        return value
+    
+    
+class VerifySerializer(serializers.Serializer):
+    token = serializers.CharField()
+    
+    def validate_token(self, value):
+        if not value:
+            raise serializers.ValidationError("Token is required")
+        return value
