55c68ddec5546d2b601fe362bf0275841d7207de
amirali-lll <amirali.dst.lll@gmail.com>
2024-06-01 21:41:50 +0330

    Add field to wallet to know then payment completed

--- wallets/models.py
+++ wallets/models.py
@@ -39,6 +39,7 @@ class Payment(models.Model):
     token = models.CharField(max_length=255)
     amount = models.DecimalField(max_digits=12, decimal_places=0)
     user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='payments')
+    verified = models.BooleanField(default=False)
     
     created_at = models.DateTimeField(auto_now_add=True)
     
