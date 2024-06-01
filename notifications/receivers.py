9e44a9f5bd43469837c4b44387eaa0a6ced14fa3
amirali-lll <amirali.dst.lll@gmail.com>
2024-06-01 23:42:49 +0330

    chore: add receiver to handle the notification creation

--- notifications/receivers.py
+++ notifications/receivers.py
@@ -1,7 +1,12 @@
+import json
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.utils.translation import gettext_lazy as _
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
 from .models import Notification
+from .serializers import NotificationSerializer
+
 
 @receiver(post_save)
 def create_notification(sender, instance, created, **kwargs):
@@ -22,4 +27,17 @@ def create_notification(sender, instance, created, **kwargs):
             user=instance.user,
             title=_(f"Payment Received"),
             message=_(f"Your payment of {instance.amount} in {instance.created_at} has been received.")
+        )
+
+
+@receiver(post_save, sender=Notification)
+def send_notification(sender, instance, created, **kwargs):
+    if created:
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            f"notify_{instance.user.id}",
+            {
+                "type": "send_notification",
+                "message": NotificationSerializer(instance).data
+            }
         )
